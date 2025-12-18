edb78f5aaa62f637aff6e3d2b155f1b3ef3a0833
Mevin-JR <supposedlysamyt@gmail.com>
2025-12-18 16:22:57 +0530

    Init progress save

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import "./App.css";
 function App() {
 
   return (
-    <main>
-      <h1>This is a test code</h1>
+    <main className="w-screen h-screen flex items-center justify-center">
+      <div className="flex items-center justify-center p-5 rounded-lg shadow-[0_0_25px_10px_rgba(0,0,0,0.25)] bg-[#e9e9e9]">
+        <h1>Login</h1>
+        
+      </div>
     </main>
   );
 }
