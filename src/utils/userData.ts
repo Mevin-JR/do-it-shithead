18e39fc5873d3472af83718b2bb45a55e674dc0e
Mevin JR <supposedlysamyt@gmail.com>
2025-12-27 10:59:44 +0530

    Dynamic email censor amount

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,4 +1,7 @@
-export const censorEmail = (email: string): string => {
+export const censorEmail = (
+  email: string,
+  censorAmount: number = 5
+): string => {
   if (email.length <= 2) {
     return email;
   }
@@ -6,7 +9,7 @@ export const censorEmail = (email: string): string => {
   const addrFirstChar = email[0];
   const addrLastChar = email[email.indexOf("@") - 1];
   const mailDomain = email.substring(email.indexOf("@"), email.length);
-  const censoredMiddle = "*".repeat(5);
+  const censoredMiddle = "*".repeat(censorAmount);
 
   return addrFirstChar + censoredMiddle + addrLastChar + mailDomain;
 };
