a36dc552598c3806e62a531ad19190a503e40222
Mevin-JR <supposedlysamyt@gmail.com>
2025-12-31 02:09:55 +0530

    Unfinished menu changes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./app.css";
 
 import { Toaster } from "react-hot-toast";
 import Login from "./components/login";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SignUp from "./components/signUp";
 import Loading from "./components/loading";
 import ThemeSwitcher from "./components/themeSwitcher";
@@ -11,17 +11,32 @@ import { Navigate } from "react-router-dom";
 
 type AuthMode = "login" | "signup";
 
-export default function App() {
-  const { user, loading } = useAuth();
+type MenuItem = {
+  label: string;
+  action: () => void;
+};
 
+export default function App() {
   const [mode, setMode] = useState<AuthMode>("login");
   const [pageLoading, setPageLoading] = useState<boolean>(false);
+  const [menu, setMenu] = useState<{
+    x: number;
+    y: number;
+    items: MenuItem[];
+  } | null>(null);
 
+  const { user, loading } = useAuth();
   if (loading) return <Loading />;
-
   if (user) {
     return <Navigate to="/home" replace />;
   }
+
+  useEffect(() => {
+    const disable = (e: MouseEvent) => e.preventDefault();
+    window.addEventListener("contextmenu", disable);
+
+    return () => window.removeEventListener("contextmenu", disable);
+  }, []);
 
   return (
     <main className="relative w-screen h-screen flex items-center justify-center bg-white text-black dark:bg-[#121212] dark:text-white smooth-transition">
